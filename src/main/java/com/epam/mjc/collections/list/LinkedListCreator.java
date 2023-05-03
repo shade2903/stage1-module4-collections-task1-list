6ff1572d4920e788b5cab33d400a569ca922bd17
shade2903 <nighshade2903@gmail.com>
2023-05-03 16:45:14 +0300

    completed

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> numbers = new LinkedList<>();
+
+        for(Integer number : sourceList){
+            if(number % 2 == 0 && number !=0){
+                numbers.addLast(number);
+            }else {
+                numbers.addFirst(number);
+            }
+        }
+        return numbers;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,37 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
+    ListComparator comparator = new ListComparator();
+
     public void sort(List<String> sourceList) {
+        Collections.sort(sourceList, comparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aInt = Integer.parseInt(a);
+        int bInt = Integer.parseInt(b);
+        int funA = function(aInt);
+        int funB = function(bInt);
+        if (funA > funB) {
+            return 1;
+        }
+        if (funA < funB) {
+            return -1;
+        }
+        if (funA == funB) {
+            return aInt - bInt;
+        }
+        return 0;
+    }
+
+    Integer function(int x) {
+        return 5 * x * x + 3;
     }
 }
